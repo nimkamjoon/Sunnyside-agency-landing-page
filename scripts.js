3784bb814f7a570cb65018cec0c5937180d82cb6
Jota Ernst <ju.swc25@gmail.com>
2021-06-20 04:37:19 -0300

    Wrote script to show/hide mobile menu.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 let button = document.getElementById("menuIcon");
+let menu = document.getElementById("menuBlock");
+
+button.addEventListener("click", showMenu)
+
+var clickCounter = 0;
+
+function showMenu() {
+    clickCounter += 1;     
+    
+    if (clickCounter > 0 && clickCounter % 2 != 0) {
+        menu.style.visibility = "visible";
+    }
+    
+    else if (clickCounter % 2 == 0) {
+        menu.style.visibility = "hidden";
+    }
+}
